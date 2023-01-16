d11099a9bbda64d47504734e517f2fd49caeb02a
carlostornez <carlostornez3@gmail.com>
2023-01-15 21:18:11 -0600

    Unit error in histograms fixed

--- detector.cc
+++ detector.cc
@@ -1,5 +1,5 @@
 #include "detector.hh"
-
+#include "G4UnitsTable.hh"
 MySensitiveDetector::MySensitiveDetector(G4String name):G4VSensitiveDetector(name)
 {
 
@@ -15,8 +15,8 @@ G4bool MySensitiveDetector::ProcessHits(G4Step *aStep, G4TouchableHistory *ROhis
     G4StepPoint *postStepPoint=aStep->GetPostStepPoint();
     G4ThreeVector posPhoton=preStepPoint->GetPosition();
     G4ThreeVector momPhoton=preStepPoint->GetMomentum();
-    G4double wlen= (1.239841939*eV/momPhoton.mag())*1E+003;
-    G4double energy=(1.239841939/(wlen*1E-003));
+    G4double energy =momPhoton.mag();
+    G4double wlen= ((1.239841939*eV*um)/momPhoton.mag());
     //Only if Multithreaded mode is not being used
     #ifndef G4MULTITHREADED
     G4cout<<"Photon position"<<posPhoton<<G4endl;
@@ -24,11 +24,14 @@ G4bool MySensitiveDetector::ProcessHits(G4Step *aStep, G4TouchableHistory *ROhis
 
 
     G4AnalysisManager *man = G4AnalysisManager::Instance();
-
+    G4double n=0.33*um;
     G4int evt =G4RunManager::GetRunManager()->GetCurrentEvent()->GetEventID();
+    G4cout<<"Wavelength: "<<G4BestUnit(wlen,"Length")<<G4endl;
+    G4cout<<"Energy: "<<G4BestUnit(energy,"Energy")<<G4endl;
     
+
     man->FillH1(1,wlen);
     man->FillH1(2,evt);
     man->FillH1(3,energy);
-    man->FillH2(0,energy, wlen);
+   man->FillH2(0,energy, wlen);
 return true;}
--- run.cc
+++ run.cc
@@ -2,25 +2,25 @@
  //Creation of histograms
  
 MyRunAction::MyRunAction(){
-    G4AnalysisManager *man =G4AnalysisManager::Instance();
+   G4AnalysisManager *man =G4AnalysisManager::Instance();
   
-   man->CreateH1("fEdep","Energy Deposition by Muons",30,0.,12*MeV,"MeV");
+   man->CreateH1("fEdep","Energy Deposition by Muons",60,0.,12*MeV,"MeV");
    man->SetH1XAxisTitle(0,"Energy Deposition (MeV)");
    man->SetH1YAxisTitle(0,"Number of Muons");
 
-   man->CreateH1("fWlength","Photon Wavelength",30,350,520,"nm");
+   man->CreateH1("fWlength","Photon Wavelength",60,350*nm,520*nm,"nm");
    man->SetH1XAxisTitle(1,"Wavelength (nm)");
    man->SetH1YAxisTitle(1,"Number of Photons");
    
-   man->CreateH1("fEvent","Amount of Photons produced per event",30,0,1000,"none");
+   man->CreateH1("fEvent","Amount of Photons produced per event",60,0,1000,"none");
    man->SetH1XAxisTitle(2,"Event");
    man->SetH1YAxisTitle(2,"Number of Photons");
    
-   man->CreateH1("fEnergy","Photon Energy",30,2.4,3.5);
+   man->CreateH1("fEnergy","Photon Energy",60,2.4*eV,3.5*eV,"eV");
    man->SetH1XAxisTitle(3,"Energy (eV)");
    man->SetH1YAxisTitle(3,"Number of Photons");
 
-   man->CreateH2("fEnergy vs fWlength","Photon Energy vs Photon Wavelength",30,2.4,3.5,30,350,520);
+   man->CreateH2("fEnergy vs fWlength","Photon Energy vs Photon Wavelength",60,2.4*eV,3.5*eV,60,350*nm,520*nm,"eV","nm");
    man->SetH2XAxisTitle(0,"Photon Energy (eV)");
    man->SetH2YAxisTitle(0,"Photon Wavelength (nm)");
    man->SetH2ZAxisTitle(0,"Number of Photons");
@@ -35,7 +35,7 @@ void MyRunAction::BeginOfRunAction(const G4Run * run){
     std::stringstream strRunID;
     strRunID<<runID;
 
-    G4String file="output"+strRunID.str()+".root";
+    G4String file="outputh"+strRunID.str()+".root";
     man->OpenFile(file);
     
 
